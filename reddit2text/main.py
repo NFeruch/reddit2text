8378c8f17d0a6a9a833c559e7c7da927ff56cbb8
Ethan Johnson <97696910+ethanpv34@users.noreply.github.com>
2024-06-25 18:15:37 -0400

    Allow input of url array (#5)
    * updated readme
    
    * simplify wording of readme
    
    * readme formatting
    
    * changes to readme
    
    * add more to the readme
    
    * - cleanup output formatting
    - add handling of max_comment_depth = 0
    - display total number of comments
    - switch from using post ID to URL
    
    * - add new planned features
    - highlight reddit app creation step
    
    * sync version number
    
    * update readme
    
    * update readme
    
    * readme link to github issues directly
    
    * update readme example url + output
    
    * fix license name in readme
    
    * fix readme
    
    * change feeding -> prompting when talking about LLMs
    
    * change textual to NLP in readme
    
    * - add .env support
    - extend logic for 0 max_comment_depth
    
    * Allows input of urls to textualize_post function
    
    ---------
    
    Co-authored-by: Nicholas Hansen-Feruch <nicholas.feruch@gmail.com>

--- reddit2text/main.py
+++ reddit2text/main.py
@@ -1,20 +1,50 @@
 import praw
-from typing import Optional
+import re
+from typing import Optional, Literal, Union, List
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Reddit2Text:
 	def __init__(
 		self,
-		client_id: str,
-		client_secret: str,
-		user_agent: str,
+		client_id: str = None,
+		client_secret: str = None,
+		user_agent: str = None,
 		*,
+		format: Optional[Literal['txt', 'json', 'csv']] = 'txt',
 		max_comment_depth: Optional[int] = None,
-		comment_delim: str = "|",
+		comment_delim: Optional[str] = "|",
 		save_output_to: Optional[str] = None
 	) -> None:
-		self.client_id = client_id
-		self.client_secret = client_secret
-		self.user_agent = user_agent
+		"""
+		Parameters
+		----------
+		client_id : str
+			Generated with your Reddit API app
+		client_secret : str
+			Generated with your Reddit API app
+		user_agent : str
+			Tells the Reddit API who you are, of the form, `<app type>:<app name>:<version> (by <your username>)`
+		format : Literal['txt', 'json', 'csv'], optional
+			How you want the Reddit thread to be displayed, by default 'txt'
+		max_comment_depth : int, optional
+			Maximum depth of comments to output, including the top-most comment. Choose 0 to exclude all comments, by default None or -1 to include all
+		comment_delim : str, optional
+			String/character used to indent comments according to their nesting level, by default "|"
+		save_output_to : str, optional
+			The location in which you want to save the output, by default None
+		"""
+		# Optionally fetch the credentials from the environment variables
+		self.client_id = client_id or os.getenv('REDDIT_CLIENT_ID')
+		self.client_secret = client_secret or os.getenv('REDDIT_CLIENT_SECRET')
+		self.user_agent = user_agent or os.getenv('REDDIT_USER_AGENT')
+
+		if not all((self.client_id, self.client_secret, self.user_agent)):
+			raise ValueError("Please provide client_id, client_secret, and user_agent")
+
+		self.format = format
 		self.max_comment_depth = max_comment_depth
 		self.comment_delim = comment_delim
 		self.save_output_to = save_output_to
@@ -32,6 +62,11 @@ class Reddit2Text:
 
 	def _process_comments(self, comments: praw.models.comment_forest.CommentForest, depth: int = 1) -> str:
 		comments_str = ""
+
+		# A max_comment_depth of 0 means only the original post will be returned
+		if self.max_comment_depth == 0:
+			return comments_str
+
 		for comment in comments:
 
 			# Skip any 'MoreComments' objects
@@ -45,10 +80,10 @@ class Reddit2Text:
 			prefix = f"{self.comment_delim} " * depth
 
 			# Safe-guard for deleted comments where the author would be None
-			author = comment.author.name if comment.author else "deleted"
+			author = comment.author.name if comment.author else "[deleted]"
 			score = comment.score
 			upvotes_or_downvotes = 'upvotes' if score >= 0 else 'downvotes'
-			comment_body = comment.body.replace('\n', '\\n')  # Replace newlines to avoid breaking the tree structure
+			comment_body = re.sub(r'\n+', ' ', comment.body)  # Replace newlines to avoid breaking the tree structure
 
 			comments_str += f"{prefix}{author} ({score} {upvotes_or_downvotes}): {comment_body}\n"
 
@@ -57,35 +92,52 @@ class Reddit2Text:
 
 		return comments_str
 
-	def _process_original_post(self, post: praw.models.Submission) -> None:
+	def _process_original_post(self, thread: praw.models.Submission) -> str:
 		# Fetch the title, author, upvotes, and post text
 		# OP's info
-		title = post.title
-		author = post.author.name if post.author else "deleted"
-		upvotes = post.score
-		selftext = post.selftext.replace('\n', ' ')  # Replace newlines to avoid breaking the structure
+		self.post_data = {
+			'title': thread.title,
+			'author': thread.author.name if thread.author else "deleted",
+			'upvotes': thread.score,
+			'selftext': re.sub(r'\n+', ' ', thread.selftext),  # Replace newlines to avoid breaking the structure
+			'num_comments': thread.num_comments
+		}
 
 		# Ensure all comments are fetched
-		post.comments.replace_more(limit=None)
+		if self.max_comment_depth != 0:
+			thread.comments.replace_more(limit=None)
 
 		# Start building the final output string
-		original_post_output = f"title: {title}\nauthor/upvotes: {author}/{upvotes}\npost text: {selftext}\n\n"
+		original_post_output = f"Title: {self.post_data['title']}\nAuthor: {self.post_data['author']}\nUpvotes: {self.post_data['upvotes']}\n"
+		if self.post_data['selftext']:
+			original_post_output += f"Body text: {self.post_data['selftext']}\n"
 
 		return original_post_output
 
-	def textualize_post(self, url: str) -> str:
-		# Extract the post ID from the URL
-		post_id = url.split('/')[-3]
-		post = self._praw_reddit.submission(id=post_id)
+	def textualize_post(self, urls: Union[str, List[str]]) -> Union[str, List[str]]:
+		if isinstance(urls, str):
+				urls = [urls]
 
-		# Convert the original post and all the comments to text individually
-		text_post = self._process_original_post(post)
-		text_comments = self._process_comments(post.comments)
+		final_outputs = []
 
-		# Combine the original post and comments into a single string
-		final_output = text_post + text_comments
+		for url in urls:
+				# PRAW auto-handles extracting the post ID from the URL
+				# https://praw.readthedocs.io/en/stable/code_overview/models/submission.html
+				thread = self._praw_reddit.submission(url=url)
 
-		# Handle the output based on the user's preference
-		# self._handle_output(final_output)
+				# Convert the original post and all the comments to text individually
+				text_post = self._process_original_post(thread)
 
-		return final_output
+				# Ensure all comments are fetched
+				text_comments = self._process_comments(thread.comments)
+
+				comment_header = f'\n{self.post_data["num_comments"]} Comments:\n--------\n' if self.max_comment_depth != 0 else ''
+
+				# Combine the original post and comments into a single string
+				final_output = text_post + comment_header + text_comments
+
+				final_outputs.append(final_output)
+
+		if len(final_outputs) == 1:
+				return final_outputs[0]
+		return final_outputs
